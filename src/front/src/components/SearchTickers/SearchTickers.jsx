7ca6bb6cd21e160fe910183d5f61d08404644cdf
André Stanlley <stanlley41@gmail.com>
2022-04-15 21:38:23 -0300

    Primeiros seletores criados

--- src/front/src/components/SearchTickers/SearchTickers.jsx
+++ src/front/src/components/SearchTickers/SearchTickers.jsx
@@ -4,28 +4,72 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 
 export default function SearchTickers() {
-    const limiter = 28
-
     const [search, setSearch] = useState()
     const [data, setData] = useState([]);
+    const [firstSeletor, setFirstSeletor] = useState(0)
+    const [secondSeletor, setSecondSeletor] = useState(0)
+    const [lastSeletor, setLastSeletor] = useState(0)
 
-    useEffect(async() => {
+    useEffect(async () => {
         const response = await axios.get("./api/tickers"); // https://api-cotacao-b3.labdo.it/api/empresa
         setData(response.data)
     }, [])
 
-    function handleSearchTicker(busca){
-        setSearch( data.filter( ( value ) => value.DescricaoDoAtivo[0].Codigo.includes(busca.toUpperCase()) 
-        || value.DescricaoDoAtivo[0].NomeMercado.includes(busca.toUpperCase())))
+
+    function handleSeletorFilter(e) {
+        // Função de busca por digitação
+        if (e.search) {
+            let busca = e.search
+            setSearch(data.filter((value) => value.DescricaoDoAtivo[0].Codigo.includes(busca.toUpperCase())
+                || value.DescricaoDoAtivo[0].NomeMercado.includes(busca.toUpperCase())))
+        }
+
+        switch (e.target.name) {
+            case "SEL1":
+                setFirstSeletor(e.target.value)
+                break;
+            case "SEL2":
+                setSecondSeletor(e.target.value)
+                break;
+            case "SEL3":
+                setLastSeletor(e.target.value)
+                break;
+        }
+
+        setSearch(data.map((value) => {
+            if(value.ValorDeMercado[0]){
+                if (value.ValorDeMercado[0].ValorDeMercado >= firstSeletor) {
+                    return value
+                }
+            }
+        }))
     }
+
 
     return (
         <div className='search-container'>
             <div id='searchTickers'>
-                <input type="text" placeholder='Buscar ticker' onChange={e => handleSearchTicker(e.target.value)}></input>
+                <input type="text" placeholder='Buscar ticker' onChange={e => handleSeletorFilter({ search: e.target.value })}></input>
                 <img src="./assets/imgs/lupa.png" alt="Lupa" />
             </div>
-            <TickerListing data={ search ? search : data.slice(0,limiter) } />
+            <form onChange={e => handleSeletorFilter(e)}>
+                <select name="SEL1" id="">
+                    <option value="10000">Valor 1</option>
+                    <option value="100000" selected>Valor 2</option>
+                    <option value="433000000000">Valor 3</option>
+                </select>
+                <select name="SEL2" id="">
+                    <option value="valor1">Valor 24</option>
+                    <option value="valor2" selected>Valor 2</option>
+                    <option value="valor3">Valor 3</option>
+                </select>
+                <select name="SEL3" id="">
+                    <option value="valor1">Valor 1</option>
+                    <option value="valor2" selected>Valor 2</option>
+                    <option value="valor3">Valor 3</option>
+                </select>
+            </form>
+            <TickerListing data={search ? search : data} />
         </div>
     )
 }
--- src/front/src/components/SearchTickers/TickerListing.jsx
+++ src/front/src/components/SearchTickers/TickerListing.jsx
@@ -2,14 +2,15 @@ import { Ticker } from './Ticker';
 import './tickerListing.css';
 
 export default function TickerListing(props) {
+  const limiter = 25
   return (
     <>
       <div className='container-tickers'>
-        {props.data.map((ticker, index) => {
+        {props.data.slice(0, limiter).map((ticker, index) => {
           try{
             return <Ticker key={index} info={{
               name: ticker.InfoEmpresaDadosGerais[0] ? ticker.InfoEmpresaDadosGerais[0].NomeEmpresarial : ticker.DescricaoDoAtivo[0].NomeMercado,
-              ticker: ticker.DescricaoDoAtivo[0] ? ticker.DescricaoDoAtivo[0].Codigo : "Indisponivel",
+              ticker: ticker.DescricaoDoAtivo[0].Codigo,
               variacao: ticker.Oscilacoes[0] ? ticker.Oscilacoes[0].Var : "",
               setor: ticker.ClassificacaoSetorial[0] ? ticker.ClassificacaoSetorial[0].Setor : "Indisponivel"
             }} />
