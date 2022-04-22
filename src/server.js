a751b7f3a7196d19f7f55739772ec438a2f6e0a9
André Stanlley <stanlley41@gmail.com>
2022-04-22 12:59:49 -0300

    port changed

--- src/server.js
+++ src/server.js
@@ -24,7 +24,7 @@ https
     app
   )
   .listen(8080, () => {
-    console.log("Servidor rodando na porta 80");
+    console.log("Servidor rodando na porta 8080");
     saveTickersInMemory.start()
     setInterval(() => {
         saveTickersInMemory = []
