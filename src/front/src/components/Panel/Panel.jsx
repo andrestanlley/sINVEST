aea46b0eb668ac1c59a1ee21d2d8a4f79a42a7b7
André Stanlley <stanlley41@gmail.com>
2022-05-02 10:34:51 -0300

    botão consertado

--- src/front/src/components/Panel/Panel.jsx
+++ src/front/src/components/Panel/Panel.jsx
@@ -8,10 +8,10 @@ export default function Panel() {
                 <div>
                     <h1>Quem somos</h1>
                     <span>Dispomos de todas as empresas listadas na Bolsa de Valores Brasileira para você realizar a melhor escolha de investimentos, através dos principais dados corporativos e indicadores econômicos, colhidos do próprio banco de dados da B3 e Comissão de Valores Mobiliários (CVM). Tudo isso de forma clara e simplificada para sua melhor experiência, possibilitando assim uma análise fundamentalista precisa e eficaz.  </span><br></br>
-                    <Link to="/indices"><button>Ver empresas listadas</button></Link>
+                    <Link to="/ativos"><button>Ver empresas listadas</button></Link>
                 </div>
                 <div id='logo'>
-                    <img src="../../assets/imgs/Logo_001.png" alt="" />
+                    <img src="../../assets/imgs/Logo_001.png" alt="Logo Visão Macro" />
                 </div>
             </PanelConteiner>
         </div>
