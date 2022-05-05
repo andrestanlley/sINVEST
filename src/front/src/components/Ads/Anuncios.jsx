0d476fd835c103ac1971e6aeb146ff9d052ef8f7
André Stanlley <stanlley41@gmail.com>
2022-05-05 08:53:37 -0300

    react helmet adicionado

--- src/front/src/components/Ads/Anuncios.jsx
+++ src/front/src/components/Ads/Anuncios.jsx
@@ -1,3 +1,17 @@
+import { Helmet } from "react-helmet";
+
 export default function Ads() {
-    return <meta name="propeller" content="fb2f9e80a4a5908fe269efc130041e5f"></meta>
+    return (
+        <Helmet>
+            <meta name="propeller" content="fb2f9e80a4a5908fe269efc130041e5f" />
+            <meta charset="utf-8" />
+            <meta name="language" content="pt-BR" />
+            <meta name="description"
+                content="Através dos principais dados corporativos e indicadores econômicos. Tudo isso de forma clara e simplificada para sua melhor experiência." />
+            <meta name="robots" content="all" />
+            <meta name="author" content="André Stanlley" />
+            <meta name="keywords"
+                content="investimento, b3, bolsa de valores, ações, visao macro, visaomacro.com, ver cotações, cotações" />
+        </Helmet>
+    )
 }
