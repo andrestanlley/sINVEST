b24921dbb0f84de0c2f4a47605720a1a34d9505d
André Stanlley <stanlley41@gmail.com>
2022-06-07 22:45:25 -0300

    tentativa de 404

--- src/routes/blog.js
+++ src/routes/blog.js
@@ -4,9 +4,6 @@ const path = require('path')
 
 router.get('/admin', (req,res) => res.redirect("https://unsteadfast-currenc.000webhostapp.com/wp-login.php"))
 router.get('/', express.static(path.resolve("src/front/dist")))
-router.get('/:article/:post', (req,res)=>{
-    console.log(req.params.article, req.params.post)
-    res.send(path.resolve("src/front/dist"))
-})
+router.get('*', (req, res)=> express.static(path.resolve("src/front/dist")));
 
 module.exports = router
